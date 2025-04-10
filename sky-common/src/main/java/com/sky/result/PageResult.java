12db1e3341b3ff7c5de78cbf1f33a5b27ebdd41c
Kiipp <a810581261@gmail.com>
2025-04-10 14:11:14 +0100

    员工分页查询业务代码开发

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -3,6 +3,7 @@ package com.sky.result;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.Serializable;
 import java.util.List;
